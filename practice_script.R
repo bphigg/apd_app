10f4389135a91fe5678d45c7a9329178e186b168
bphigg <bphigg@gmail.com>
2026-03-20 18:13:07 -0400

    2nd push

--- practice_script.R
+++ practice_script.R
@@ -1,5 +1,12 @@
 library(tidyverse)
+library(stringr)
 
-arrests <- read_csv("../../db/apd/APD_Arrests.csv")
+arrests <- read_csv("apd/APD_Arrests.csv")
 head(arrests)
 str(arrests)
+
+help(mutate)
+
+arrests <- arrests %>%
+  mutate(address=str_remove_all(address, "-BLK")) %>%
+  mutate(address=paste(address, "Asheville, NC"))
