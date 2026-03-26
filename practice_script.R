2e8c3ef1ce37337fa1cc2a0a7267d4ce02cc67f5
bphigg_macair <bphigg@gmail.com>
2026-03-26 09:46:10 -0400

    update3_26

--- practice_script.R
+++ practice_script.R
@@ -9,4 +9,15 @@ help(mutate)
 
 arrests <- arrests %>%
   mutate(address=str_remove_all(address, "-BLK")) %>%
-  mutate(address=paste(address, "Asheville, NC"))
+  mutate(city='Asheville') %>%
+  mutate(state='NC')
+
+
+pd_force1 <- read_csv("apd/APD_Use_Of_Force_2021-2024.csv")
+str(pd_force1)
+pd_force2 <- read_csv("apd/APD_Use_Of_Force_2018-2020_data.csv")
+str(pd_force2)
+attributes(pd_force1)$names
+attributes(pd_force2)$names
+
+length(unique(pd_force1$objectid_1)) == length(pd_force$objectid_1)
